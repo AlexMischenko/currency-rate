0a74afe9ae4069835b7ef068364a34c6bdd97861
AlexMischenko <god.hates.us.all2001@gmail.com>
2019-09-11 18:11:50 +0300

    minor fix

--- src/Screens/CurrencyDetail/index.js
+++ src/Screens/CurrencyDetail/index.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import { StyleSheet, ScrollView, View, TouchableOpacity, Text } from 'react-native'
+
+import { getCrypoCurrencyDetails } from '../../businessLogic'
 
 const CurrencyDetail = ({ navigation }) => {
+  const [details, setDetails] = useState(null)
   const currencyId = navigation.getParam('currencyId')
 
+  useEffect(() => {
+    async function getDetails() {
+      const fetchedDetails = await getCrypoCurrencyDetails(currencyId)
+      console.log('TCL: getDetails -> details', details)
+      setDetails(fetchedDetails)
+    }
+
+    getDetails()
+  })
+
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1 }}>
       <Text style={{}}>{`CurrencyDetail Page ${currencyId}`}</Text>
-    </View>
+      <Text style={{}}>{JSON.stringify(details)}</Text>
+    </ScrollView>
   )
 }
 
